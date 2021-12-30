0299886c33de7d9a05bdd50f4546df8a859bcdad
Alexei Semenenko <48149971+AlexeSemenenko@users.noreply.github.com>
2021-12-31 00:03:13 +0300

    Create Class1.cs

---
+++ Lab2/Class1/Class1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Class1
+{
+    public class Class1
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello world from 1st class");
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("Bye-bye world :(");
+        }
+    }
+}
