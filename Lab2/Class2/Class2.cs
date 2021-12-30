31ddf91145a7f1f3f058e0ceaac6595f8d07ab79
Alexei Semenenko <48149971+AlexeSemenenko@users.noreply.github.com>
2021-12-31 00:04:13 +0300

    Create Class2.cs

---
+++ Lab2/Class2/Class2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Class2
+{
+    public class Class2
+    {
+        public static void Fact()
+        {
+            Console.WriteLine("2 + 2 * 2 = 6");
+        }
+
+        public static void Сompliment()
+        {
+            Console.WriteLine("You are a wonderful person");
+        }
+    }
+}
