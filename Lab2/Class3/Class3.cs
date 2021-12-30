4370a959bcd61904792624beb2a97f87253ddf2e
Alexei Semenenko <48149971+AlexeSemenenko@users.noreply.github.com>
2021-12-31 00:04:40 +0300

    Create Class3.cs

---
+++ Lab2/Class3/Class3.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Class3
+{
+    public class Class3
+    {
+        public static void Last()
+        {
+            Console.WriteLine("Last method from last (3rd) class");
+        }
+    }
+}
